9207ad1d4ecd6b402a0612bf2b8dbefb21b379ce
SemenTeplov <eburg-severca@mail.ru>
2026-01-16 18:00:22 +0500

    fix: fixed

--- ewm-service/src/main/java/ru/practicum/service/CompilationServerImpl.java
+++ ewm-service/src/main/java/ru/practicum/service/CompilationServerImpl.java
@@ -32,7 +32,6 @@ import ru.practicum.openapi.model.UserShortDto;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
